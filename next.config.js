d8e30cba7be3b15a49ac942b881fef8fd8082e8d
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-24 17:48:25 +0300

    configured styled-components

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+const withTM = require("next-transpile-modules")([
+  "@mui/material",
+  "@mui/system",
+  "@mui/icons-material", // If @mui/icons-material is being used
+]);
+
+module.exports = withTM({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+    return config;
+  },
+});
