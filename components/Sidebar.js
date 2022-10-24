b2d8effd7e03b7782e5ddfe5f3bdc78af7281a03
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-24 18:34:24 +0300

    Header part of the Sidebar is coming up slowly. The Search is also done and now heading to the start chat action

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,15 +1,67 @@
-import styled from "styled-components"
+import { Avatar, IconButton } from "@mui/material";
+import styled from "styled-components";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Sidebar() {
   return (
     <Container>
-        <Header></Header>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
     </Container>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
 
-const Container = styled.div``
+const Container = styled.div``;
 
-const Header = styled.div``
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background-color: white;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: solid 1px whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex:1;
+`;
