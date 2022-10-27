15539c88796a8ffd5867c283eef7a65d54be2773
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-27 05:28:20 +0300

    If the user record does not exist, it will be created. If the record does exist, its contents will be overwritten with the newly provided data.

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,47 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../services/firebase";
-import Login from './login';
-import Loading from '../components/Loading';
+import {
+  auth,
+  db,
+  doc,
+  ref,
+  serverTimestamp,
+  set,
+  setDoc,
+} from "../services/firebase";
+import Login from "./login";
+import Loading from "../components/Loading";
+import { useEffect } from "react";
+import { getDatabase } from "firebase/database";
 
 function MyApp({ Component, pageProps }) {
-
   const [user, loading] = useAuthState(auth);
+  // const db = getDatabase()
 
-  if(loading) return <Loading/>;
-  if(!user) return <Login/>;
+  useEffect(() => {
+    if (user) {
+      // set(ref(db, "users/" + user.uid), {
+      //   email: user.email,
+      //   lastSeen: serverTimestamp(),
+      //   photoURL: user.photoURL,
+      // });
+      const userRef = doc(db, "users/" + user.uid);
+      setDoc(
+        userRef,
+        {
+          email: user.email,
+          lastSeen: serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
 
-  return <Component {...pageProps} />
+  if (loading) return <Loading />;
+  if (!user) return <Login />;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
