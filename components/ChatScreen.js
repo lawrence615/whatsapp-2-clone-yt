57c966ba5aef0fcb0ef2f4a5982283a376a42cdd
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-30 13:00:20 +0300

    working on loading the messages and having an input to type in the message

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,22 +1,131 @@
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { Avatar, IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from '@mui/icons-material/Mic';
+import { useCollection } from "react-firebase-hooks/firestore";
 
+import Message from "./Message";
+import {
+  auth,
+  collection,
+  db,
+  doc,
+  orderBy,
+  query,
+} from "../services/firebase";
 
-
-import { auth } from "../services/firebase";
-
-
-function ChatScreen({chat, messages}) {
-
+function ChatScreen({ chat, messages }) {
   const [user] = useAuthState(auth);
-  const router = useRouter()
+  const router = useRouter();
+  const [messagesSnashot] = useCollection(
+    query(
+      collection(doc(db, "chats", router.query.id), "messages"),
+      orderBy("timestamp", "asc")
+    )
+  );
 
-  return <Container><h1>ChatScreen</h1></Container>;
+  const showMessages = () => {
+    if (messagesSnashot) {
+      return messagesSnashot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
+
+  return (
+    <Container>
+      <Header>
+        <Avatar />
+        <HeaderInformation>
+          <h3>Recipient Email</h3>
+          <p>Last seen...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+      <MessageContainer>
+        {showMessages()}
+        <EndOfMessage />
+      </MessageContainer>
+      <InputContainer>
+        <InsertEmoticonIcon />
+        <Input />
+        <MicIcon/>
+      </InputContainer>
+    </Container>
+  );
 }
 
 export default ChatScreen;
 
-const Container = styled.div`
+const Container = styled.div``;
 
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div``;
+
+const EndOfMessage = styled.div``;
+
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: white;
+  z-index: 100;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  outline: 0;
+  border:none;
+  border-radius: 10px;
+  background-color: whitesmoke;
+  padding: 20px;
+  margin-left: 15px;
+  margin-right: 15px;
 `;
