4293729f47be6693b04d0db8a57220617fa247bc
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-30 06:44:47 +0300

    added enterChat function which takes you to a chat room once you click any of chat mates.

--- components/Chat.js
+++ components/Chat.js
@@ -3,11 +3,15 @@ import { Avatar } from "@mui/material";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { query } from "firebase/database";
+import { useRouter } from "next/router";
+
 
 import getRecipientEmail from "../utils/getRecipientEmail";
 import { auth, collection, db, where } from "../services/firebase";
 
+
 function Chat({ id, users }) {
+  const router = useRouter()
   const [user] = useAuthState(auth);
   const [recipientSnapshot] = useCollection(
     query(
@@ -16,12 +20,15 @@ function Chat({ id, users }) {
     )
   );
 
+  const enterChat = () => {
+    router.push(`/chat/${id}`);
+  }
+
   const recipient = recipientSnapshot?.docs?.[0]?.data();
-  console.log('recipient',recipient)
   const recipientEmail = getRecipientEmail(users, user);
 
   return (
-    <Container>
+    <Container onClick={enterChat}>
       {recipient ? (
         <UserAvatar src={recipient?.photoURL} />
       ) : (
