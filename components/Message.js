40936055a95e709790a1a72eacd8c2455e99a42a
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-31 06:29:03 +0300

    Update Message.js
    timestamp value added on each message.

--- components/Message.js
+++ components/Message.js
@@ -1,16 +1,22 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import moment from "moment";
 
 import { auth } from "../services/firebase";
 
 function Message({ user, message }) {
   const [userLoggedIn] = useAuthState(auth);
 
-  const TypeOfMessage = user === userLoggedIn.email ? Sender : Receiver
+  const TypeOfMessage = user === userLoggedIn.email ? Sender : Receiver;
 
   return (
     <Container>
-      <TypeOfMessage>{message.message}</TypeOfMessage>
+      <TypeOfMessage>
+        {message.message}
+        <Timestamp>
+          {message.timestamp ? moment(message.timestamp).format("LT") : "..."}
+        </Timestamp>
+      </TypeOfMessage>
     </Container>
   );
 }
@@ -38,4 +44,14 @@ const Sender = styled(MessageElement)`
 const Receiver = styled(MessageElement)`
   background-color: whitesmoke;
   text-align: left;
+`;
+
+const Timestamp = styled.span`
+  color: gray;
+  padding: 10px;
+  font-size: 9px;
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 0;
 `;
